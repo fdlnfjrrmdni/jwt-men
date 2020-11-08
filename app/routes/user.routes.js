101be7bf2c6c466e86d7bd659950516747a7a58b
Fadlan Fajar Ramdani <fdlnfjrrmdni@gmail.com>
2020-11-08 22:19:01 +0700

    undo

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -4,7 +4,6 @@ const controller = require("../controllers/user.controller");
 module.exports = function(app) {
 	app.use(function(req, res, next) {
 		res.header(
-			"Access-Control-Allow-Origin", "*",
 			"Access-Control-Allow-Headers",
 			"x-access-token, Origin, Content-Type, Accept"
 		);
