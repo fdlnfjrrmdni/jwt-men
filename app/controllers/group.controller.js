01040bb28394e09ff3579b8f00457c1ce350f016
Fadlan Fajar Ramdani <fdlnfjrrmdni@gmail.com>
2020-11-10 15:46:58 +0700

    add route group

--- app/controllers/group.controller.js
+++ app/controllers/group.controller.js
@@ -13,3 +13,16 @@ exports.findAll = (req, res) => {
 	    });
   	});
 };
+
+exports.findAll = (req, res) => {
+  	const name = req.query.name;
+  	let condition = name ? { name: { $regex: new RegExp(name), $options: "i" } } : {};
+
+  	Group.find(condition).then(data => {
+  		res.send(data);
+  	}).catch(err => {
+  		res.status(500).send({
+	        message: err.message || "Some error occurred while retrieving goods."
+	    });
+  	})
+};
